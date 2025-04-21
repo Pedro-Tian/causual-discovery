1c6934c175a20c2544a7afd425225e31df00d8b0
root <root@train-3131018-worker-0.train-3131018.ai.svc>
2025-04-21 19:43:55 +0800

    update

--- exp_test.sh
+++ exp_test.sh
@@ -31,30 +31,30 @@ python DC_FAS.py --model DAS --repeat 10 --nodes 50 --h 3 --type SF
 python DC_FAS.py --model DAS --repeat 10 --nodes 50 --h 4 --type SF
 python DC_FAS.py --model DAS --repeat 10 --nodes 50 --h 5 --type SF
 
-python DC_FAS.py --model DAS --repeat 10 --nodes 100 --h 2 
-python DC_FAS.py --model DAS --repeat 10 --nodes 100 --h 3
-python DC_FAS.py --model DAS --repeat 10 --nodes 100 --h 4
-python DC_FAS.py --model DAS --repeat 10 --nodes 100 --h 5
-python DC_FAS.py --model DAS --repeat 10 --nodes 100 --h 2 --type SF
-python DC_FAS.py --model DAS --repeat 10 --nodes 100 --h 3 --type SF
-python DC_FAS.py --model DAS --repeat 10 --nodes 100 --h 4 --type SF
-python DC_FAS.py --model DAS --repeat 10 --nodes 100 --h 5 --type SF
+python DC_FAS.py --model DAS --repeat 5 --nodes 100 --h 2 
+python DC_FAS.py --model DAS --repeat 5 --nodes 100 --h 3
+python DC_FAS.py --model DAS --repeat 5 --nodes 100 --h 4
+python DC_FAS.py --model DAS --repeat 5 --nodes 100 --h 5
+python DC_FAS.py --model DAS --repeat 5 --nodes 100 --h 2 --type SF
+python DC_FAS.py --model DAS --repeat 5 --nodes 100 --h 3 --type SF
+python DC_FAS.py --model DAS --repeat 5 --nodes 100 --h 4 --type SF
+python DC_FAS.py --model DAS --repeat 5 --nodes 100 --h 5 --type SF
 
-python DC_FAS.py --model DAS --repeat 10 --nodes 300 --h 2 
-python DC_FAS.py --model DAS --repeat 10 --nodes 300 --h 3
-python DC_FAS.py --model DAS --repeat 10 --nodes 300 --h 4
-python DC_FAS.py --model DAS --repeat 10 --nodes 300 --h 5
-python DC_FAS.py --model DAS --repeat 10 --nodes 300 --h 2 --type SF
-python DC_FAS.py --model DAS --repeat 10 --nodes 300 --h 3 --type SF
-python DC_FAS.py --model DAS --repeat 10 --nodes 300 --h 4 --type SF
-python DC_FAS.py --model DAS --repeat 10 --nodes 300 --h 5 --type SF
+python DC_FAS.py --model DAS --repeat 5 --nodes 300 --h 2 
+python DC_FAS.py --model DAS --repeat 5 --nodes 300 --h 3
+python DC_FAS.py --model DAS --repeat 5 --nodes 300 --h 4
+python DC_FAS.py --model DAS --repeat 5 --nodes 300 --h 5
+python DC_FAS.py --model DAS --repeat 5 --nodes 300 --h 2 --type SF
+python DC_FAS.py --model DAS --repeat 5 --nodes 300 --h 3 --type SF
+python DC_FAS.py --model DAS --repeat 5 --nodes 300 --h 4 --type SF
+python DC_FAS.py --model DAS --repeat 5 --nodes 300 --h 5 --type SF
 
-python DC_FAS.py --model DAS --repeat 10 --nodes 500 --h 2 
-python DC_FAS.py --model DAS --repeat 10 --nodes 500 --h 3
-python DC_FAS.py --model DAS --repeat 10 --nodes 500 --h 4
-python DC_FAS.py --model DAS --repeat 10 --nodes 500 --h 5
-python DC_FAS.py --model DAS --repeat 10 --nodes 500 --h 2 --type SF
-python DC_FAS.py --model DAS --repeat 10 --nodes 500 --h 3 --type SF
-python DC_FAS.py --model DAS --repeat 10 --nodes 500 --h 4 --type SF
-python DC_FAS.py --model DAS --repeat 10 --nodes 500 --h 5 --type SF
+python DC_FAS.py --model DAS --repeat 5 --nodes 500 --h 2 
+python DC_FAS.py --model DAS --repeat 5 --nodes 500 --h 3
+python DC_FAS.py --model DAS --repeat 5 --nodes 500 --h 4
+python DC_FAS.py --model DAS --repeat 5 --nodes 500 --h 5
+python DC_FAS.py --model DAS --repeat 5 --nodes 500 --h 2 --type SF
+python DC_FAS.py --model DAS --repeat 5 --nodes 500 --h 3 --type SF
+python DC_FAS.py --model DAS --repeat 5 --nodes 500 --h 4 --type SF
+python DC_FAS.py --model DAS --repeat 5 --nodes 500 --h 5 --type SF
 
