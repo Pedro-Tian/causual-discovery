aeafaf674ec6ec1bbb616834f327ea6d4dd990d7
Pedro-Tian <tianpengchao@bilibili.com>
2025-04-27 10:48:17 +0800

    update

--- exp_mergelam_golem.bat
+++ exp_mergelam_golem.bat
@@ -1,27 +1,36 @@
 @REM python DC_FAS.py --model GOLEM --repeat 10 --nodes 10 --h 2
-@REM python DC_FAS.py --model GOLEM --repeat 10 --nodes 10 --h 3
-@REM python DC_FAS.py --model GOLEM --repeat 10 --nodes 10 --h 4
+python DC_FAS.py --model GOLEM --repeat 10 --nodes 10 --h 3
+python DC_FAS.py --model GOLEM --repeat 10 --nodes 10 --h 4
 @REM python DC_FAS.py --model GOLEM --repeat 10 --nodes 10 --h 5
-@REM python DC_FAS.py --model GOLEM --repeat 10 --nodes 10 --h 2 --type SF
-python DC_FAS.py --model GOLEM --repeat 10 --nodes 10 --h 5 --type SF
-@REM python DC_FAS.py --model GOLEM --repeat 10 --nodes 10 --h 3 --type SF
-@REM python DC_FAS.py --model GOLEM --repeat 10 --nodes 10 --h 4 --type SF
+python DC_FAS.py --model GOLEM --repeat 10 --nodes 10 --h 2 --type SF
+@REM python DC_FAS.py --model GOLEM --repeat 10 --nodes 10 --h 5 --type SF
+python DC_FAS.py --model GOLEM --repeat 10 --nodes 10 --h 3 --type SF
+python DC_FAS.py --model GOLEM --repeat 10 --nodes 10 --h 4 --type SF
 
-@REM python DC_FAS.py --model GOLEM --repeat 10 --nodes 50 --h 2
-@REM python DC_FAS.py --model GOLEM --repeat 10 --nodes 50 --h 3
-@REM python DC_FAS.py --model GOLEM --repeat 10 --nodes 50 --h 4
-python DC_FAS.py --model GOLEM --repeat 10 --nodes 50 --h 5 --type SF
-@REM python DC_FAS.py --model GOLEM --repeat 10 --nodes 50 --h 2 --type SF
-python DC_FAS.py --model GOLEM --repeat 10 --nodes 50 --h 5
-@REM python DC_FAS.py --model GOLEM --repeat 10 --nodes 50 --h 3 --type SF
-@REM python DC_FAS.py --model GOLEM --repeat 10 --nodes 50 --h 4 --type SF
+python DC_FAS.py --model GOLEM --repeat 10 --nodes 30 --h 2
+python DC_FAS.py --model GOLEM --repeat 10 --nodes 30 --h 3
+python DC_FAS.py --model GOLEM --repeat 10 --nodes 30 --h 4
+python DC_FAS.py --model GOLEM --repeat 10 --nodes 30 --h 5 --type SF
+python DC_FAS.py --model GOLEM --repeat 10 --nodes 30 --h 2 --type SF
+python DC_FAS.py --model GOLEM --repeat 10 --nodes 30 --h 5
+python DC_FAS.py --model GOLEM --repeat 10 --nodes 30 --h 3 --type SF
+python DC_FAS.py --model GOLEM --repeat 10 --nodes 30 --h 4 --type SF
+
+python DC_FAS.py --model GOLEM --repeat 10 --nodes 50 --h 2
+python DC_FAS.py --model GOLEM --repeat 10 --nodes 50 --h 3
+python DC_FAS.py --model GOLEM --repeat 10 --nodes 50 --h 4
+@REM python DC_FAS.py --model GOLEM --repeat 10 --nodes 50 --h 5 --type SF
+python DC_FAS.py --model GOLEM --repeat 10 --nodes 50 --h 2 --type SF
+@REM python DC_FAS.py --model GOLEM --repeat 10 --nodes 50 --h 5
+python DC_FAS.py --model GOLEM --repeat 10 --nodes 50 --h 3 --type SF
+python DC_FAS.py --model GOLEM --repeat 10 --nodes 50 --h 4 --type SF
 
 @REM python DC_FAS.py --model GOLEM --repeat 10 --nodes 100 --h 2
 @REM python DC_FAS.py --model GOLEM --repeat 10 --nodes 100 --h 3
 @REM python DC_FAS.py --model GOLEM --repeat 10 --nodes 100 --h 4
 @REM python DC_FAS.py --model GOLEM --repeat 10 --nodes 100 --h 5
 @REM python DC_FAS.py --model GOLEM --repeat 10 --nodes 100 --h 2 --type SF
-python DC_FAS.py --model GOLEM --repeat 10 --nodes 100 --h 5 --type SF
+@REM python DC_FAS.py --model GOLEM --repeat 10 --nodes 100 --h 5 --type SF
 @REM python DC_FAS.py --model GOLEM --repeat 10 --nodes 100 --h 3 --type SF
 @REM python DC_FAS.py --model GOLEM --repeat 10 --nodes 100 --h 4 --type SF
 
